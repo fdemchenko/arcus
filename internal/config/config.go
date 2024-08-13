5702f11ab664d1b3d0bca342a6d3f8f91c967a68
Filipp Demchenko <filipp5632@gmail.com>
2024-08-13 13:00:57 +0300

    Add configuration to project

---
+++ internal/config/config.go
@@ -0,0 +1,57 @@
+package config
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Storage    StorageConfig `yaml:"storage"`
+	HTTPServer HTTPConfig    `yaml:"http-server"`
+	OpenAIKEY  string        `yaml:"openai-key" env:"ARCUS_OPENAI_KEY" env-required:"true"`
+	Env        string        `yaml:"env" env-default:"development"`
+}
+
+type HTTPConfig struct {
+	Host string `yaml:"host" env-default:"localhost"`
+	Port int    `yaml:"port" env-default:"8080"`
+}
+
+type StorageConfig struct {
+	DSN string `yaml:"dsn" env-required:"true" env:"ARCUS_POSTGRES_DSN"`
+}
+
+func MustLoad() *Config {
+	cfg, err := Load()
+	if err != nil {
+		panic("error loading config: " + err.Error())
+	}
+	return cfg
+}
+
+func Load() (*Config, error) {
+	configPath, err := fetchConfigPath()
+	if err != nil {
+		return nil, err
+	}
+	var cfg Config
+	err = cleanenv.ReadConfig(configPath, &cfg)
+	if err != nil {
+		return nil, err
+	}
+	return &cfg, nil
+}
+
+func fetchConfigPath() (string, error) {
+	var configPath string
+	flag.StringVar(&configPath, "config-path", os.Getenv("ARCUS_CONFIG_PATH"), "App configuration file path")
+	flag.Parse()
+
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		return "", fmt.Errorf("cannot find config path: %w", err)
+	}
+	return configPath, nil
+}
