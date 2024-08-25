f50142190c0c285508cd6b72dc0602e48a85dee0
Filipp Demchenko <filipp5632@gmail.com>
2024-08-25 19:33:19 +0300

    Separate handlers to different files by domain model

---
+++ internal/app/posts.go
@@ -0,0 +1,62 @@
+package app
+
+import (
+	"log/slog"
+	"net/http"
+	"strings"
+
+	"github.com/fdemchenko/arcus/internal/api/request"
+	"github.com/fdemchenko/arcus/internal/api/response"
+	"github.com/fdemchenko/arcus/internal/models"
+	"github.com/fdemchenko/arcus/internal/validator"
+)
+
+func (app *Application) createPost(w http.ResponseWriter, r *http.Request) {
+	const op = "app.routes.createPost"
+	logger := app.logger.With(slog.String("op", op))
+
+	var input struct {
+		Title   string   `json:"title"`
+		Content *string  `json:"content"`
+		Tags    []string `json:"tags"`
+	}
+	err := request.ReadJSON(r.Body, &input)
+	if err != nil {
+		logger.Error("failed to decode JSON user input", slog.String("error", err.Error()))
+		response.SendError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	post := models.Post{
+		Title: strings.TrimSpace(input.Title),
+	}
+	if input.Content != nil {
+		trimmed := strings.TrimSpace(*input.Content)
+		post.Content = &trimmed
+	}
+	if input.Tags == nil {
+		input.Tags = make([]string, 0)
+	}
+	for i := range len(input.Tags) {
+		input.Tags[i] = strings.TrimSpace(input.Tags[i])
+	}
+	post.Tags = input.Tags
+
+	v := validator.New()
+	if post.Validate(v); !v.IsValid() {
+		logger.Error("failed to validate incoming post", slog.Any("errors", v.Errors))
+		response.SendError(w, http.StatusBadRequest, v.Errors)
+		return
+	}
+
+	postID, err := app.postsService.Create(post)
+	if err != nil {
+		logger.Error("failed to create post", slog.String("err", err.Error()))
+		response.SendServerError(w)
+		return
+	}
+
+	if err := response.WriteJSON(w, http.StatusCreated, response.Envelope{"post_id": postID}); err != nil {
+		response.SendServerError(w)
+	}
+}
