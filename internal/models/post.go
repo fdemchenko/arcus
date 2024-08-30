6732dd30fa5e799cfb49809b208551a20f34f749
Filipp Demchenko <filipp5632@gmail.com>
2024-08-30 14:15:25 +0300

    Add preparing routine to simplify client code and change handling of empty content

--- internal/models/post.go
+++ internal/models/post.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"fmt"
+	"strings"
 	"time"
 	"unicode/utf8"
 
@@ -17,23 +18,41 @@ type Post struct {
 	Title     string    `json:"title"`
 	Content   *string   `json:"content"`
 	Tags      []string  `json:"tags"`
+	Version   int       `json:"version"`
 	UserID    int       `json:"user_id"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
 }
 
 func (p *Post) Validate(v validator.Validator) {
+	p.preparePost()
+
 	v.Check(p.Title != "", "title", "must not be empty")
 	v.Check(utf8.RuneCountInString(p.Title) <= PostTitleMaxLength, "title", fmt.Sprintf("must not be greater than %d characters long", PostTitleMaxLength))
-
-	if p.Content != nil {
-		v.Check(*p.Content != "", "description", "must not be empty")
-	}
 
 	v.Check(len(p.Tags) <= PostTagsMaxAmount, "tags", fmt.Sprintf("max tags amount is %d", PostTagsMaxAmount))
 
 	for _, tag := range p.Tags {
 		v.Check(tag != "", "tag", "must not be empty")
 		v.Check(utf8.RuneCountInString(tag) <= PostTagMaxLength, "tag", fmt.Sprintf("must not be greater than %d characters long", PostTagMaxLength))
+	}
+}
+
+func (p *Post) preparePost() {
+	p.Title = strings.TrimSpace(p.Title)
+
+	if p.Content != nil {
+		trimmed := strings.TrimSpace(*p.Content)
+		p.Content = &trimmed
+		if *p.Content == "" {
+			p.Content = nil
+		}
+	}
+
+	if p.Tags == nil {
+		p.Tags = make([]string, 0)
+	}
+	for i := range len(p.Tags) {
+		p.Tags[i] = strings.TrimSpace(p.Tags[i])
 	}
 }
